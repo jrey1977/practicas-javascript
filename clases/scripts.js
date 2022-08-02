c468897602aaca6b3c3daf60709190151a89ad75
Javier <jrey1977@yahoo.es>
2022-08-02 07:27:39 +0200

    Commit dormido

--- clases/scripts.js
+++ clases/scripts.js
@@ -2,13 +2,22 @@
 listadoEstudiantes = [];
 listadoProfesores = [];
 
+// Otras cosas
+let idsObjetos = 0;
+const fragment = document.createDocumentFragment();
+
 // Clases
 class Persona {
     constructor(nombre, edad, tipo) {
         this.nombre = nombre;
         this.edad = edad;
         this.tipo = tipo;
+        this.id = this.obtener_id();
     }
+    obtener_id = function () {
+        idsObjetos++;
+        return idsObjetos;
+    };
 }
 
 class Estudiante extends Persona {
@@ -25,10 +34,52 @@ class Estudiante extends Persona {
 
 window.onload = function () {
     // Selectores
+
+    // Botón agregar
     btnAgregar = document.getElementById("btnAgregar");
+
+    // Formulario
     oForm = document.forms[0];
 
+    // Plantilla Estudiante y Profesor
+    const liEstudiante = document.querySelector("#liEstudiante");
+    const estudiante = liEstudiante.cloneNode(true);
+    const liProfesor = document.querySelector("#liProfesor");
+    const profesor = liProfesor.cloneNode(true);
+
+    // Listado Estudiantes y oprofesores
+    const listadoEstudiantesSelector = document.querySelector(
+        "#listadoEstudiantes"
+    );
+    const listadoProfesoresSelector =
+        document.querySelector("#listadoProfesores");
+
+    // Fragment: Réplica ligera del DOM
+    const fragment = document.createDocumentFragment();
+
     // Funciones
+    function meterObjeto(nombre, edad, tipoPersona, idPersona) {
+        plantillaTipoPersona =
+            "li" + (tipoPersona[0].toUpperCase() + tipoPersona.substring(1));
+        console.log("plantillaTipoPersona", plantillaTipoPersona);
+        const tipoPersonaSelector = document.querySelector(
+            `#${plantillaTipoPersona}`
+        );
+        const clone = tipoPersonaSelector.content.cloneNode(true);
+        console.log("clone", clone);
+        clone.querySelector(".nombrePersona").textContent = nombre;
+        clone.querySelector(".categoriaPersona").textContent = tipoPersona;
+        clone.querySelector(".edadPersona").textContent = edad;
+        clone.querySelector(".idPersona").value = idPersona;
+
+        fragment.appendChild(clone);
+        if (tipoPersona === "estudiante") {
+            listadoEstudiantesSelector.appendChild(fragment);
+        } else {
+            listadoProfesoresSelector.appendChild(fragment);
+        }
+    }
+
     oForm.addEventListener("submit", (e) => {
         e.preventDefault();
         // Recogemos los valores del formulario
@@ -37,12 +88,20 @@ window.onload = function () {
         let tipoPersona = oForm.elements["tipo"].value;
         if (tipoPersona === "estudiante") {
             let estudiante = new Estudiante(nombre, edad, tipoPersona);
+            idPersona = estudiante.id;
+            console.log("La id es ", idPersona);
             listadoEstudiantes.push(estudiante);
         } else {
             let profesor = new Persona(nombre, edad, tipoPersona);
+            idPersona = profesor.id;
+            console.log("La id es ", idPersona);
             listadoProfesores.push(profesor);
         }
+
+        meterObjeto(nombre, edad, tipoPersona, idPersona);
         console.log("Listado de estudiantes:", listadoEstudiantes);
         console.log("Listado de profesores:", listadoProfesores);
+
+        // TO DO: Meter elementos en listados
     });
 };
